3e7c2518c8b18465131866edd116728b1d8184ff
lsalvadori-pc <lorenzo.salvadori.86@gmail.com>
2020-02-01 10:23:37 +0000

    camera movement added

---
+++ Assets/Sources/CameraMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+    public GameObject target; // player to follow
+    public Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = targetPosition.x + cameraOffset.x;
+        cameraPosition.y = targetPosition.y + cameraOffset.y;
+        transform.position = cameraPosition;
+    }
+}
