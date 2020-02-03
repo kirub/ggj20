db1ac94c1beca0cca56ee3cac192de3a98f4df08
KiruB <kirubb@gmail.com>
2020-02-03 14:57:58 +0100

    (really) add soul HUD

---
+++ Assets/ScreenUIComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenUIComponent : MonoBehaviour
+{
+    public static ScreenUIComponent HUD = null; 
+    public SoulGaugeBehaviour SoulGauge { get; private set; } = null;
+
+    private void Awake()
+    {
+        HUD = this;
+        SoulGauge = GetComponentInChildren<SoulGaugeBehaviour>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SoulGaugeBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulGaugeBehaviour : MonoBehaviour
+{
+    private const int MaxSoulValue = 3;
+    private int CurrentSoulValue = MaxSoulValue;
+    private Animator[] UISoulAnims = null;
+
+    private void Awake()
+    {
+        UISoulAnims = GetComponentsInChildren<Animator>();
+        SetSoulsCount(3);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DelaySetActiveFalse(int Idx)
+    {
+        yield return new WaitForSeconds(0.2f);
+        UISoulAnims[Idx].SetBool("IsActive", false);
+    }
+
+    public void SetSoulsCount(int SoulValue)
+    {
+        SoulValue = Mathf.Clamp(SoulValue, 1, 3 );
+        int PreviousSoulValue = CurrentSoulValue;
+        CurrentSoulValue = SoulValue;
+
+        if( CurrentSoulValue != PreviousSoulValue )
+        {
+            for( int Idx = Mathf.Max(PreviousSoulValue, CurrentSoulValue) - 1; Idx > Mathf.Min(PreviousSoulValue, CurrentSoulValue) - 1; --Idx )
+            {
+                UISoulAnims[Idx].SetTrigger("Fade");
+                StartCoroutine(DelaySetActiveFalse(Idx));
+            }
+        }
+
+        UISoulAnims[CurrentSoulValue - 1].SetBool("IsActive", true);
+    }
+}
